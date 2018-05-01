bf78522f690a75564d0dbf7d64601529ee86bdd9
John-AZ1 <jeremy@exemail.com.au>
2018-05-01 15:13:51 +1000

    Added more information and made session return html obsolete

--- Day.py
+++ Day.py
@@ -8,5 +8,6 @@ class Day(list):
     def return_html(self):
         html = "<h1>{}</h1><h3>Week {}</h3>".format(self.day_name, self.week_id)
         for session in self:
-            html += "{}<br>".format(session.return_html())
+            html += "<code>{}</code><teacher>{}</teacher><room>{}</room><time>{}<time>"
+                .format(session.code, session.teacher, session.room, session.time)
         return html
