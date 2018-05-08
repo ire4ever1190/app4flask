c962b42fdc62ce4eb51406a65dafa6c5cc451643
Jake Leahy <jakeleahy@gmail.com>
2018-05-08 20:32:18 +1000

    Say Hi! to tox everyone he will be unit testing this now along with pytest

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# This file is only needed for tox which is what is used for unit testing
+from distutils.core import setup
+
+setup(name='App4Flask',
+      version='0.1',
+      description='Flask app to display timetable',
+      author='Jake Leahy',
+      author_email='darhyaust@gmail.com',
+      url='idonthaveone',
+      #packages=['distutils', 'distutils.command'],
+     )
