bc1d72f41a1ad12f534d34c43f7239987cc0dae4
Jake Leahy <jakeleahy@gmail.com>
2018-05-09 20:27:05 +1000

    Added test fie

---
+++ test.py
@@ -0,0 +1,28 @@
+import app
+import os
+import datetime
+
+username = str(os.environ["username"])
+password = str(os.environ["password"])
+
+def test_info():
+        today = datetime.datetime.today().weekday()
+        classes = []
+        for i in range(1, 10):
+                classes.append("<class>" + str(app.data.get(today, i, username, "class")) + "</class>")
+                timetablefordaylist = ''.join(classes)
+        assert app.show_info(username, password) == timetablefordaylist
+
+
+def test_extrainfo():
+        today = datetime.datetime.today().weekday()
+        classes = []
+        items = ["class", "time", "teacher", "room"]
+        for i in range(1, 10):
+                for x in items:
+                        # Formats the info into tags e.g. <teacher> #Teacher name# </teacher>
+                        info = "<{}>{}</{}>".format(x, app.data.get(today, i, username, x), x)
+                        classes.append(info)
+        timetablefordaylist = ''.join(classes)
+        assert app.show_extrainfo(username, password) == timetablefordaylist
+
