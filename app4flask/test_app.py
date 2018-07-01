527baf45d12a626b93f2a286aee66fc1db6cf193
Jake Leahy <jakeleahy@gmail.com>
2018-07-02 06:03:28 +1000

    added form testing

--- app4flask/test_app.py
+++ app4flask/test_app.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import mechanicalsoup
 
 class TestApp(object):
         def test_client_updating(self):
@@ -10,3 +11,14 @@ class TestApp(object):
                 r = requests.post(url=url, headers=headers)
                 json = r.json()
                 assert json[0]["session1"]["Info"]["Class"] != None
+
+        def test_browser(self):
+                br = mechanicalsoup.StatefulBrowser()
+                br.open("http://127.0.0.1:5000/login")
+                br.select_form(nr=0)
+                username = str(os.environ["username"])
+                password = str(os.environ["password"])
+                br["username"] = username
+                br["password"] = password
+                br.submit_selected()
+                assert br.get_current_page().find('title').text.strip() is not "Title"
